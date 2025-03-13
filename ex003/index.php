b116c35b53700663adb18120f965bebbbe74f98c
RobertoSantos <robertocoesantos@gmail.com>
2025-03-13 12:43:03 -0300

    Curso PHP

---
+++ ex003/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos de primitivos em PHP</title>
+</head>
+<body>
+    <h1>Tste de tipos primitivos</h1>
+    <?php
+        // $num = 010;
+        // echo "O valor da variável é $num";
+        // $num = 3e2;
+        // echo "O valor de $num"; //3 x 10(2)
+
+        // $v = 300;
+        // var_dump($v);
+
+        // $v = 45.6;
+        // var_dump($v);
+
+        // $v = true;
+        // var_dump($v);
+
+        // $v = "Roberto";
+        // var_dump($v);
+
+        // $num = (integer) 3e2; //coerção
+        // var_dump($num);
+
+        // $num = (float) "950";
+        // var_dump($num);
+
+        // $casado = false;
+        // echo "Eu sou $casado";
+
+        // $casado = true;
+        // echo "Eu sou $casado";
+
+        // $vet = [6, 2, 9, 3, 5];
+        // var_dump($vet);
+
+        // class Pessoa{
+        //     private string $nome;
+        // }
+        // $p = new Pessoa;
+        // var_dump($p);
+
+         $nome = "Roberto";
+$sobrenome = "Santos";
+$ap = "Lindao";
+  echo "$nome \"ap\" $sobrenome";
+?>
+</body>
+</html>
