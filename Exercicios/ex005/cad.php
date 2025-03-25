c2d34299f3acdca0ea3e6c1eb99fd7e14371314e
RobertoSantos <robertocoesantos@gmail.com>
2025-03-25 16:02:11 -0300

    Atualizando pojeto

---
+++ Exercicios/ex005/cad.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css"> 
+    
+</head>
+<body>
+    <header>
+        <h1>Resultado do Processamento</h1>
+    </header>
+    <main>
+        <?php
+            $num = "ceil(454)"; 
+            echo($num);
+        ?>
+        <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
+    </main>
+    
+</body>
+</html>
---
+++ Exercicios/ex005/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Interação com Formulários</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado final</h1>
+        <section>
+            <form action="cad.php" method="get">
+                <label for="Número">Nome</label> 
+                <input type="text" name="Número" id="idNúmero">
+                <input type="submit" value="Calcular">
+            </form>
+        </section>
+    </header>
+</body>
+</html>
